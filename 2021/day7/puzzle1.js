dc1ba244b18d9eb31e40e7dcb0bbf30aecf58191
Jedde Bowman <jedde.j.bowman1@aexp.com>
2021-12-07 10:48:37 -0700

    Implementing day 7

---
+++ 2021/day7/puzzle1.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const path = require("path");
+
+let points = fs
+    .readFileSync(path.join(__dirname, "input.txt"), { encoding: "utf-8" })
+    .trim()
+    .split(",")
+    .map(Number)
+    .sort();
+
+// O(m*n) where m is the range of the dataset, and n is the size of the dataset
+let bestCost, bestPoint;
+for (let i = points[0]; i <= points[points.length - 1]; i++) {
+    let cost = 0;
+    points.forEach((point) => {
+        cost += Math.abs(point - i);
+    });
+    if (!bestCost || bestCost > cost) {
+        bestCost = cost;
+        bestPoint = i;
+    }
+}
+console.log({ bestCost, bestPoint });
---
+++ 2021/day7/puzzle2.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const path = require("path");
+
+let points = fs
+    .readFileSync(path.join(__dirname, "input.txt"), { encoding: "utf-8" })
+    .trim()
+    .split(",")
+    .map(Number)
+    .sort();
+
+let getCost = (p1, p2) => {
+    let difference = Math.abs(p1 - p2);
+    return (difference * (difference + 1)) / 2;
+};
+
+// O(m*n) where m is the range of the dataset, and n is the size of the dataset
+let bestCost, bestPoint;
+for (let i = points[0]; i <= points[points.length - 1]; i++) {
+    let cost = 0;
+    points.forEach((point) => {
+        cost += getCost(point, i);
+    });
+    if (!bestCost || bestCost > cost) {
+        bestCost = cost;
+        bestPoint = i;
+    }
+}
+console.log({ bestCost, bestPoint });
